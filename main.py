ae97ccd726251cd50dcc401d0dc8446980491690
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-03 18:39:35 -0500

    calling article input function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from openai.sentiment_analysis import analyze_sentiment
 from my_alpaca.trading import submit_order
 from data.fetch_articles import fetch_articles
+from data.fetch_articles import article_input
 from data.process_articles import process_articles
 from models.trading_strategy import decide_trades
 from dotenv import load_dotenv
@@ -16,11 +17,13 @@ GPT_API_KEY = os.getenv("GPT_API_KEY")
 ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
 ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
 
-
-
 def main():
     # Step 1: Fetch and process articles
-    articles = fetch_articles()
+
+    # Gather URL
+    
+    url = article_input()
+    articles = fetch_articles(url)
     processed_articles = process_articles(articles)
 
     # Step 2: Analyze sentiment of articles
