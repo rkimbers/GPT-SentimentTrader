a7df5bd346abc91e1341a2360c4d07043bbbfb1a
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-03 14:13:01 -0500

    made process article function work with full article text

--- data/process_articles.py
+++ data/process_articles.py
@@ -1,12 +1,29 @@
-# file: data/process_articles.py
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import re
+
+# Function to filter out invisible HTML elements
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
 
 def process_articles(raw_articles):
-    # Here, you might want to clean the text, remove HTML tags, stop words, perform stemming, etc.
-
     processed_articles = []
-    for article in raw_articles:
-        text = article.get_text()  # extract text from the HTML of the article
-        # perform additional processing here
-        processed_articles.append(text)
+    
+    for raw_article in raw_articles:
+        soup = BeautifulSoup(raw_article, 'html.parser')
+        texts = soup.findAll(text=True)
+        visible_texts = filter(tag_visible, texts)
+
+        # Join all the visible text fragments together
+        full_article_text = " ".join(t.strip() for t in visible_texts)
+
+        # Optional: Remove extra whitespaces and newlines
+        full_article_text = re.sub(r'\s+', ' ', full_article_text)
+
+        processed_articles.append(full_article_text)
 
     return processed_articles
