2fad133b1c11b803c8490f701395cc492b9e55c4
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-03 17:58:19 -0500

    switching GPT to return both ticker and sentiment

--- openai/sentiment_analysis.py
+++ openai/sentiment_analysis.py
@@ -1,53 +1,62 @@
 # sentiment_analysis.py
 import os
-from openai import OpenAI
+import openai
 
 # Get the OpenAI API key from environment variables
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # Use the OpenAI API key to authenticate
-OpenAI.api_key = openai_api_key
+openai.api_key = openai_api_key
 
-def analyze_sentiment(processed_articles):
+def analyze_sentiment(articles):
     """
     Function to analyze the sentiment of a list of articles using OpenAI's GPT API.
 
     Parameters:
-    processed_articles: A list of dictionaries. Each dictionary represents an article and has a 'content' key and a 'symbol' key.
+    articles: A list of dictionaries. Each dictionary represents an article and has a 'content' key.
 
     Returns:
     A dictionary with stock symbols as keys and sentiment scores as values.
     """
     scores = {}
 
-    for article in processed_articles:
-        # Extract the article content and symbol
+    for article in articles:
+        # Extract the article content
         content = article['content']
-        symbol = article['symbol']
 
         # Prepare the prompt for the GPT API
-        prompt = f"This is a sentiment analysis request. Please evaluate the sentiment of the following text: \"{content}\""
+        prompt = f"The following news article was found: \n\n\"{content}\"\n\n\
+        Please identify any mentioned stock tickers and the sentiment (positive, negative, neutral) associated with each ticker."
 
         # Send the prompt to the GPT API and get the response
-        response = OpenAI.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=3)
+        response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=200)
 
-        # Extract the sentiment from the response
-        sentiment = response['choices'][0]['text'].strip().lower()
+        # The model's response will be in the form 'Symbol: sentiment'
+        model_response = response.choices[0].text.strip()
 
-        # Calculate a sentiment score
-        if sentiment == 'positive':
-            score = 1
-        elif sentiment == 'negative':
-            score = -1
-        else:
-            score = 0
+        # Process the response
+        try:
+            symbol, sentiment = model_response.split(":")
+            symbol = symbol.strip()
+            sentiment = sentiment.strip().lower()
 
-        # If the symbol is already in the scores dictionary, add the new score to the existing score
-        # Otherwise, add a new entry to the scores dictionary
-        if symbol in scores:
-            scores[symbol] += score
-        else:
-            scores[symbol] = score
+            # Calculate a sentiment score
+            if sentiment == 'positive':
+                score = 1
+            elif sentiment == 'negative':
+                score = -1
+            else:
+                score = 0
+
+            # If the symbol is already in the scores dictionary, add the new score to the existing score
+            # Otherwise, add a new entry to the scores dictionary
+            if symbol in scores:
+                scores[symbol] += score
+            else:
+                scores[symbol] = score
+
+        except ValueError:
+            print(f"Could not process the model's response: {model_response}")
 
     # Return the scores dictionary
     return scores
