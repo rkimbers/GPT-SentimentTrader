abe9703301e223820e9de67096064e8c2d19fbaa
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-09 17:22:25 -0500

    added decide trades function call

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from my_openai.sentiment_analysis import analyze_sentiment
 from my_alpaca.trading import submit_order
 from data.fetch_articles import fetch_article, fetch_articles
-from data.process_articles import process_articles
+from data.process_articles import process_article  
 from models.trading_strategy import decide_trades
 from dotenv import load_dotenv
 import os
@@ -22,26 +22,27 @@ def main():
     
     # Initialize an empty list to store the processed articles
     # Fetch and process each article
-    articles = []
+    processed_articles = []
     for url in urls:
         article_html = fetch_article(url)
-        articles.append(article_html)
-
-    # Process all fetched articles
-    processed_articles = process_articles(articles)
+        processed_article = process_article(article_html)  # Updated this line
+        if processed_article is not None:  # Add this line to avoid appending None
+            processed_articles.append(processed_article)
 
     # Step 2: Analyze sentiment of articles
-    trades_to_execute = analyze_sentiment(processed_articles)
+    sentiment_scores = []
+    for article in processed_articles:
+        score = analyze_sentiment(article)
+        if score is not None:  # Add this line to avoid appending None
+            sentiment_scores.append(score)
 
     # Step 3: Decide trades based on sentiment scores
-    #trades_to_execute = decide_trades(sentiment_scores)
+    trades_to_execute = decide_trades(sentiment_scores)
 
+    print(trades_to_execute)
     # Step 4: Execute trades
-    for trade, score in trades_to_execute.items():
-        submit_order({trade: score})
+    #for trade, score in trades_to_execute.items():
+    #    submit_order({trade: score})
 
 if __name__ == "__main__":
     main()
-
-
-
