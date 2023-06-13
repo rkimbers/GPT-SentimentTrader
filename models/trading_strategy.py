b9df462163ada22955103d1cee2e7b0fe9cec43d
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-13 12:55:35 -0500

    added support for sell orders

--- models/trading_strategy.py
+++ models/trading_strategy.py
@@ -1,113 +1,94 @@
-#file: trading_strategy.py
+# file: trading_strategy.py
 from operator import itemgetter
 from alpaca_trade_api import REST
 from alpaca.trading.client import TradingClient
-from finance_utils import get_symbol 
+from finance_utils import get_symbol, get_share_price, prepare_trades, calculate_total_sentiment, translate_symbols
+from account_utils import account_value, portfolio_positions
 from dotenv import load_dotenv
-
-import os
-import requests
 from operator import itemgetter
 from math import floor
 
-ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
-ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
+import os
+import requests
 
-# Getting non-marginable account value and printing it
-def account_value():  
+def prepare_buy_orders(sentiment_scores):
+    ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
+    ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
     trading_client = TradingClient(ALPACA_API_KEY, ALPACA_SECRET_KEY, paper=True)
-    account = trading_client.get_account()
-    return float(account.non_marginable_buying_power)
-
-# Gets share price given a ticker symbol
-def get_share_price(ticker):
-    ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
-
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval=5min&apikey={ALPHA_VANTAGE_API_KEY}"
-
-    # Send the GET request and get the response
-    response = requests.get(url)
-
-    # Parse the JSON response
-    data = response.json()
-    # Ensure the response contains the key "Time Series (5min)"
-    if "Time Series (5min)" in data:
-        # Get the most recent timestamp
-        recent_timestamp = max(data["Time Series (5min)"].keys())
-
-        # Get the closing price at the most recent timestamp
-        recent_price = data["Time Series (5min)"][recent_timestamp]["4. close"]
-
-        return float(recent_price)
-    else:
-        print(f"Unable to get price for ticker {ticker}.")
-        return None
-
-
-def decide_trades(sentiment_scores):
+    
     portfolio_value = account_value()
     order_cap = portfolio_value * 0.10
 
-    # Convert company names to ticker symbols and get share prices
-    trades_preparation = []
-    for company, sentiment_score in sentiment_scores.items():
-        symbol = get_symbol(company)
-        print(f"{company} symbol: {symbol}")  # Debugging line
-        if symbol is not None:
-            share_price = get_share_price(symbol)
-            print(f"{company} share price: {share_price}")  # Debugging line
-            trades_preparation.append({
-                'symbol': symbol,
-                'sentiment_score': sentiment_score,
-                'share_price': share_price,
-            })
+    # Translate sentiment_scores keys from company names to symbols
+    sentiment_scores = {get_symbol(k): v for k, v in sentiment_scores.items()}
 
-    # Sort trades in descending order of sentiment score
-    trades_preparation.sort(key=itemgetter('sentiment_score'), reverse=True)
-
-    # Calculate the total sentiment score
-    total_sentiment = sum([trade['sentiment_score'] for trade in trades_preparation])
-
-    # Decide on trades to execute
+    trades_preparation = prepare_trades(sentiment_scores)
+    total_sentiment = calculate_total_sentiment(trades_preparation)
+    
     trades_to_execute = []
     for trade in trades_preparation:
+        symbol = trade['symbol']
         weight = trade['sentiment_score'] / total_sentiment  # Calculate weight for each stock
         allocated_money = order_cap * weight  # Allocate money based on weight
-        print(f"{trade['symbol']} allocated money: {allocated_money}")  # Debugging line
-
-        # Ensure that allocated money is not greater than available budget
-        while allocated_money > order_cap:
-            allocated_money -= trade['share_price']
 
         qty = floor(allocated_money / trade['share_price'])  # Calculate quantity to buy
-        print(f"{trade['symbol']} quantity: {qty}")  # Debugging line
 
         trades_to_execute.append({
-            'symbol': trade['symbol'],
+            'symbol': symbol,
             'qty': qty,
-            'sentiment_score': trade['sentiment_score'],
-            'share_price': trade['share_price'],
+            'side': 'buy',
+            'type': 'market',
+            'time_in_force': 'gtc'
         })
 
-        # Update the order cap
         order_cap -= qty * trade['share_price']
 
     return trades_to_execute
 
 
+def prepare_sell_orders(sentiment_scores):
+    ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
+    ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
+    trading_client = TradingClient(ALPACA_API_KEY, ALPACA_SECRET_KEY, paper=True)
+
+    # Translate sentiment_scores keys from company names to symbols
+    sentiment_scores = {get_symbol(k): v for k, v in sentiment_scores.items()}
+    
+    positions = portfolio_positions(trading_client)
+
+    sell_orders = []
+    for position in positions:
+        symbol = position['symbol']
+        score = sentiment_scores.get(symbol)
+        
+        if score is not None and score < 0:
+            fraction_to_sell = abs(score / 10)  # e.g. -2 becomes 0.2, -9 becomes 0.9
+            qty_to_sell = int(fraction_to_sell * int(position['qty']))
+
+            if qty_to_sell > 0:
+                sell_orders.append({
+                    'symbol': symbol,
+                    'qty': qty_to_sell,
+                    'side': 'sell',
+                    'type': 'market',
+                    'time_in_force': 'gtc'
+                })
+
+    return sell_orders
+
+
 
 if __name__ == '__main__':
     load_dotenv()
-    
-    ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
-    ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
-    
+
+
     sentiment_scores = {
-        'Nvidia': 8,
+        'Nvidia': -8,
         'Amazon': 5,
         'Target Corp': 5,
         'Advanced Micro Devices Inc': 2,
-        'Apple Inc': 9
+        'Apple Inc': -9  
     }
-    
-    print(decide_trades(sentiment_scores))
+
+    #print(prepare_buy_orders(sentiment_scores))
+    print(prepare_sell_orders(sentiment_scores))
