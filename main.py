1d318a4b5de5114c601b68145b9a951ad946c9a9
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-13 21:04:01 -0500

    updated for new syntax

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from my_openai.sentiment_analysis import analyze_sentiment
 from my_alpaca.trading import submit_order
 from data.fetch_articles import fetch_article, fetch_articles
 from data.process_articles import process_article  
-#from models.trading_strategy import decide_trades
+from models.trading_strategy import prepare_buy_orders, prepare_sell_orders
 from dotenv import load_dotenv
+from models.finance_utils import translate_symbols
+
 import os
 
 # Load environment variables
@@ -11,39 +13,46 @@ load_dotenv()
 
 # Access the API keys
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-ALPACA_API_KEY = os.getenv("APCA_API_KEY_ID")
+ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
 ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
 
 def main():
     # Step 1: Fetch and process articles
 
-    # Fetch the URLs of the top 5 earnings articles
+    # Fetch the URLs of the last 10 earnings articles
     urls = fetch_articles()
     
-    # Initialize an empty list to store the processed articles
+    # Initialize an empty dictionary to store the processed articles
     # Fetch and process each article
-    processed_articles = []
+    sentiment_scores = {}
     for url in urls:
         article_html = fetch_article(url)
         processed_article = process_article(article_html)  # Updated this line
-        if processed_article is not None:  # Add this line to avoid appending None
-            processed_articles.append(processed_article)
-
-    # Step 2: Analyze sentiment of articles
-    sentiment_scores = []
-    for article in processed_articles:
-        score = analyze_sentiment(article)
-        if score is not None:  # Add this line to avoid appending None
-            sentiment_scores.append(score)
+        if processed_article is not None:  # Added this line to avoid appending None
+            # Analyze sentiment of each article and update sentiment_scores dictionary
+            scores = analyze_sentiment(processed_article)
+            for k, v in scores.items():
+                if k in sentiment_scores:
+                    sentiment_scores[k].append(v)
+                else:
+                    sentiment_scores[k] = [v]
 
     print(sentiment_scores)
-    # Step 3: Decide trades based on sentiment scores
-    #trades_to_execute = decide_trades(sentiment_scores)
+    
+    # Translate sentiment_scores keys from company names to symbols
+    sentiment_scores = translate_symbols(sentiment_scores)
 
-    #print(trades_to_execute)
-    # Step 4: Execute trades
-    #for trade, score in trades_to_execute.items():
-    #    submit_order({trade: score})
+    # Step 2: Decide trades based on sentiment scores
+    buy_orders = prepare_buy_orders(sentiment_scores)
+    sell_orders = prepare_sell_orders(sentiment_scores)
+
+    # Print the orders
+    print(buy_orders)
+    print(sell_orders)
+
+    # Step 3: Execute trades
+    for order in buy_orders + sell_orders:
+        submit_order(order)
 
 if __name__ == "__main__":
     main()
