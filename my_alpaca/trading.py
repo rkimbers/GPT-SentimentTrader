061d09d082f11edea668f4fbc556a52e0a6d6600
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-16 09:58:29 -0500

    removed order for loop

--- my_alpaca/trading.py
+++ my_alpaca/trading.py
@@ -23,32 +23,39 @@ Submit an order to the Alpaca API.
 param trade: A dictionary representing the trade. It should contain the keys "symbol", "qty" and "side".
 :return: The API's response to the order.
 """
-def submit_order(trade):
-    
-    #placeholder
-    qty=1
-    
+def submit_order(order):
     ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
     ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
     
     trading_client = TradingClient(ALPACA_API_KEY, ALPACA_SECRET_KEY, paper=True)
 
-    symbol, sentiment_score = list(trade.items())[0]  # Extract the symbol and sentiment score
-    side = "buy" if sentiment_score == 1 else "hold"  # Buy if sentiment is positive, else hold
+    symbol = order['symbol']
+    qty = order['qty']
+    side = order['side']
+    type_ = order['type']
+    time_in_force = order['time_in_force']
 
-    # If the sentiment score is positive, place the order
-    if sentiment_score == 1:
-        market_order_data = MarketOrderRequest(
-            symbol=symbol,
-            qty=qty,
-            side=OrderSide.BUY if side == "buy" else OrderSide.SELL,
-            time_in_force=TimeInForce.GTC
-        )
+    # Change side to uppercase as it's expected by the SDK
+    side = side.upper()
 
-        market_order = trading_client.submit_order(market_order_data)
-        
-        for property_name, value in vars(market_order).items():
+    # Check the side is either BUY or SELL
+    if side not in ["BUY", "SELL"]:
+        print(f"Invalid side: {side} for symbol: {symbol}. Skipping...")
+        return
+
+    # Prepare the order data
+    order_data = MarketOrderRequest(
+        symbol=symbol,
+        qty=qty,
+        side=OrderSide.BUY if side == "BUY" else OrderSide.SELL,
+        type=type_,
+        time_in_force=TimeInForce.GTC if time_in_force == 'gtc' else None
+    )
+
+    # Submit the order
+    try:
+        order_response = trading_client.submit_order(order_data)
+        for property_name, value in vars(order_response).items():
             print(f"\"{property_name}\": {value}")
-        
-        return market_order
-
+    except Exception as e:
+        print(f"Failed to submit order for {symbol}. Error: {e}")
