b9b9d9e779dc8468749b4fa8008bf0faeeec00cc
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-24 16:30:20 -0600

    added immediate orders support

--- models/trading_strategy.py
+++ models/trading_strategy.py
@@ -5,6 +5,8 @@ from alpaca.trading.client import TradingClient
 from .finance_utils import get_symbol, get_share_price, prepare_trades, calculate_total_sentiment
 from .finance_utils import compile_and_average_scores, translate_symbols
 from .account_utils import account_value, portfolio_positions
+from database.db_manager import check_url_in_database, save_url_to_database
+from my_alpaca.trading import submit_order
 from dotenv import load_dotenv
 from operator import itemgetter
 from math import floor
@@ -20,8 +22,24 @@ def prepare_buy_orders(sentiment_scores):
     portfolio_value = account_value()
     order_cap = portfolio_value * 0.10
 
+    sentiment_scores_copy = sentiment_scores.copy()
+    for k, v in sentiment_scores_copy.items():
+        if check_url_in_database(k):  # Skip if this URL is already in the database
+            del sentiment_scores[k]
+            continue
+        
+        score = compile_and_average_scores(v)
+        if score == 10 or score == -10:  # If the sentiment score is 10 or -10, prepare and submit an order immediately
+            immediate_order = prepare_immediate_order(k, score, 'buy' if score > 0 else 'sell')
+            print(f"Submitting immediate order: {immediate_order}")
+            submit_order(immediate_order)
+            continue
+
+        sentiment_scores[k] = score
+        save_url_to_database(k, score)  # Save the URL and the sentiment score to the database
+
     # Translate sentiment_scores keys from company names to symbols
-    sentiment_scores = {get_symbol(k): compile_and_average_scores(v) for k, v in sentiment_scores.items()}
+    sentiment_scores = {get_symbol(k): v for k, v in sentiment_scores.items()}
 
     # Filter out sentiment_scores with negative average scores
     sentiment_scores = {k: v for k, v in sentiment_scores.items() if v > 0}
@@ -54,16 +72,11 @@ def prepare_buy_orders(sentiment_scores):
 
     return trades_to_execute
 
-
-
 def prepare_sell_orders(sentiment_scores):
     ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
     ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
     trading_client = TradingClient(ALPACA_API_KEY, ALPACA_SECRET_KEY, paper=True)
 
-    # Translate sentiment_scores keys from company names to symbols and average the scores
-    sentiment_scores = {get_symbol(k): compile_and_average_scores(v) for k, v in sentiment_scores.items()}
-    
     positions = portfolio_positions(trading_client)
 
     sell_orders = []
@@ -85,6 +98,37 @@ def prepare_sell_orders(sentiment_scores):
                 })
 
     return sell_orders
+
+
+def prepare_immediate_order(symbol, score, side):
+    ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
+    ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
+    trading_client = TradingClient(ALPACA_API_KEY, ALPACA_SECRET_KEY, paper=True)
+
+    portfolio_value = account_value()
+    order_cap = portfolio_value * 0.10
+
+    # Obtain the current price of the stock
+    share_price = get_share_price(symbol)
+    if share_price is None:
+        print(f"Skipping trade preparation for {symbol} due to inability to retrieve share price.")
+        return None
+
+    # Calculate the weight of the order and the allocated money
+    weight = abs(score) / 10  # Calculate weight based on score
+    allocated_money = order_cap * weight  # Allocate money based on weight
+
+    # Calculate the number of shares to purchase given the allocated money
+    qty = floor(allocated_money / share_price)
+
+    return {
+        'symbol': symbol,
+        'qty': qty,
+        'side': side,
+        'type': 'market',
+        'time_in_force': 'gtc'
+    }
+
 
 if __name__ == '__main__':
     load_dotenv()
