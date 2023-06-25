4b1b7522ce9c195d2ffec7c176904061853ab3e9
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-24 23:08:18 -0600

    added database support

--- database/db_manager.py
+++ database/db_manager.py
@@ -36,6 +36,10 @@ def check_url_in_database(url):
     return result is not None
 
 def save_url_to_database(url, source, symbol, sentiment_score):
+    if check_url_in_database(url):
+        print(f"URL {url} already exists in the database. Skipping...")
+        return
+
     conn = connect_db()
     c = conn.cursor()
 
@@ -62,3 +66,45 @@ def create_table_if_not_exists():
 
     conn.commit()
     conn.close()
+    
+def get_all_tables():
+    conn = connect_db()
+    c = conn.cursor()
+
+    # Execute a query to get all tables in the database
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+    # Fetch all results
+    tables = c.fetchall()
+
+    conn.close()
+
+    return tables
+
+def get_all_tables_strings():
+    conn = connect_db()
+    c = conn.cursor()
+
+    # Execute a query to get all tables in the database
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+    # Fetch all results and extract table names from the tuples
+    tables = [table[0] for table in c.fetchall()]
+
+    conn.close()
+
+    return tables
+
+def get_all_records():
+    conn = connect_db()
+    c = conn.cursor()
+
+    # Execute a query to get all records from the 'articles' table
+    c.execute("SELECT * FROM articles;")
+
+    # Fetch all results
+    records = c.fetchall()
+
+    conn.close()
+
+    return records
