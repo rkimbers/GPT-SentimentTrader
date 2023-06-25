3d762b75b949f6eb12b5ab3e1ec46d859bee1e6f
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-24 23:10:02 -0600

    updated yahoo finance scraping

--- data/fetch_articles.py
+++ data/fetch_articles.py
@@ -79,7 +79,7 @@ def fetch_articles():
 
 def yf_fetch_articles():
     base_url = "https://finance.yahoo.com"
-    topic_url = "https://finance.yahoo.com/topic/earnings"
+    topic_url = "https://finance.yahoo.com/most-active"
 
     response = requests.get(topic_url)
     soup = BeautifulSoup(response.text, 'html.parser')
