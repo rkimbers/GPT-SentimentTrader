561fab9586ba6a7afd692760c8f82acf033bc79d
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-06-30 20:27:33 -0500

    added error handling for all functions

--- database/db_manager.py
+++ database/db_manager.py
@@ -10,152 +10,113 @@ else:
     DB_NAME = 'articles.db'
 
 def connect_db():
-    return sqlite3.connect(DB_NAME)
+    try:
+        return sqlite3.connect(DB_NAME)
+    except sqlite3.Error as e:
+        print(f"Error connecting to database: {e}")
+        return None
 
 def create_table():
     conn = connect_db()
-    c = conn.cursor()
-
-    # Create table
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS articles (
-            id INTEGER PRIMARY KEY,
-            url TEXT UNIQUE,
-            source TEXT,
-            symbol TEXT,
-            sentiment_score REAL
-        )
-    ''')
-
-    conn.commit()
-    conn.close()
+    if conn is None:
+        return
+    try:
+        c = conn.cursor()
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS articles (
+                id INTEGER PRIMARY KEY,
+                url TEXT UNIQUE,
+                source TEXT,
+                symbol TEXT,
+                sentiment_score REAL
+            )
+        ''')
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Error creating table: {e}")
+    finally:
+        conn.close()
 
 def check_url_in_database(url):
     conn = connect_db()
-    c = conn.cursor()
-
-    c.execute("SELECT * FROM articles WHERE url=?", (url,))
-
-    result = c.fetchone()
-
-    conn.close()
-
-    return result is not None
+    if conn is None:
+        return False
+    try:
+        c = conn.cursor()
+        c.execute("SELECT * FROM articles WHERE url=?", (url,))
+        result = c.fetchone()
+        return result is not None
+    except sqlite3.Error as e:
+        print(f"Error querying database: {e}")
+        return False
+    finally:
+        conn.close()
 
 def save_url_to_database(url, source, symbol, sentiment_score):
     if check_url_in_database(url):
         print(f"URL {url} already exists in the database. Skipping...")
         return
-
     conn = connect_db()
-    c = conn.cursor()
-
-    c.execute("INSERT INTO articles (url, source, symbol, sentiment_score) VALUES (?, ?, ?, ?)", 
-              (url, source, symbol, sentiment_score))
-
-    conn.commit()
-    conn.close()
-    
-    print(f"New entry succssfully added: url - {url}, source - {source}, symbol - {symbol}, sentiment_score - {sentiment_score}")
-
-def create_table_if_not_exists():
-    conn = connect_db()
-    c = conn.cursor()
-
-    # Create table if not exists
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS articles (
-            id INTEGER PRIMARY KEY,
-            url TEXT UNIQUE,
-            source TEXT,
-            symbol TEXT,
-            sentiment_score REAL
-        )
-    ''')
-
-    conn.commit()
-    conn.close()
-    
-def get_all_tables():
-    conn = connect_db()
-    c = conn.cursor()
-
-    # Execute a query to get all tables in the database
-    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-
-    # Fetch all results
-    tables = c.fetchall()
-
-    conn.close()
-
-    return tables
-
-def get_all_tables_strings():
-    conn = connect_db()
-    c = conn.cursor()
-
-    # Execute a query to get all tables in the database
-    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-
-    # Fetch all results and extract table names from the tuples
-    tables = [table[0] for table in c.fetchall()]
-
-    conn.close()
-
-    return tables
-
-def get_all_records():
-    conn = connect_db()
-    c = conn.cursor()
-
-    # Execute a query to get all records from the 'articles' table
-    c.execute("SELECT * FROM articles;")
-
-    # Fetch all results
-    records = c.fetchall()
-
-    conn.close()
-
-    return records
+    if conn is None:
+        return
+    try:
+        c = conn.cursor()
+        c.execute("INSERT INTO articles (url, source, symbol, sentiment_score) VALUES (?, ?, ?, ?)",
+                  (url, source, symbol, sentiment_score))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        print(f"Duplicated URL {url}. Skipping...")
+    except sqlite3.Error as e:
+        print(f"Error saving URL to database: {e}")
+    finally:
+        conn.close()
 
 def fetch_sentiment_scores_from_database():
-    # Connect to the database
     conn = connect_db()
-    c = conn.cursor()
-
-    # Execute a query to get all records from the 'articles' table
-    c.execute("SELECT symbol, sentiment_score FROM articles;")
-
-    # Fetch all results
-    records = c.fetchall()
-
-    # Close the connection to the database
-    conn.close()
-
-    # Initialize an empty dictionary to store the sentiment scores
+    if conn is None:
+        return {}
     sentiment_scores = {}
-
-    # Go through each record
-    for record in records:
-        # Extract the company and sentiment score
-        company = record[0]
-        score = record[1]
-
-        # If the company is already in the dictionary, add the score to its list of scores
-        if company in sentiment_scores:
-            sentiment_scores[company].append(score)
-        # If the company is not in the dictionary, add a new entry with a list containing the score
-        else:
-            sentiment_scores[company] = [score]
-
+    try:
+        c = conn.cursor()
+        c.execute("SELECT symbol, sentiment_score FROM articles;")
+        records = c.fetchall()
+        for record in records:
+            company = record[0]
+            score = record[1]
+            if company in sentiment_scores:
+                sentiment_scores[company].append(score)
+            else:
+                sentiment_scores[company] = [score]
+    except sqlite3.Error as e:
+        print(f"Error fetching sentiment scores from database: {e}")
+    finally:
+        conn.close()
     return sentiment_scores
 
 def delete_all_records():
     conn = connect_db()
-    c = conn.cursor()
+    if conn is None:
+        return
+    try:
+        c = conn.cursor()
+        c.execute("DELETE FROM articles;")
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"Error deleting all records from database: {e}")
+    finally:
+        conn.close()
 
-    # Execute a query to delete all records from the 'articles' table
-    c.execute("DELETE FROM articles;")
-
-    conn.commit()
-    conn.close()
+def get_all_records():
+    conn = connect_db()
+    if conn is None:
+        return []
+    records = []
+    try:
+        c = conn.cursor()
+        c.execute("SELECT * FROM articles;")
+        records = c.fetchall()
+    except sqlite3.Error as e:
+        print(f"Error getting all records from database: {e}")
+    finally:
+        conn.close()
+    return records
