4a38902ebaa93c0a1e8be879d3b13a8ecc4fa012
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-07-04 19:12:07 -0500

    added threading/concurrent timeout

--- my_twilio/messaging.py
+++ my_twilio/messaging.py
@@ -1,23 +1,73 @@
 from twilio.rest import Client
+import concurrent.futures
 import os
 
 def create_twilio_client():
     twilio_account_sid = os.getenv("TWILIO_ACCOUNT_SID")
     twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-    return Client(twilio_account_sid, twilio_auth_token)
+    client = Client(twilio_account_sid, twilio_auth_token)
+    #client.http_client.debug = True  # turn on debug mode
+    return client
+
 
 def send_order_text(orders):
-    
-    client = create_twilio_client()
-    
-    twilio_phone_number = os.getenv("TWILIO_PHONE_NUMBER")  
-    my_phone_number = os.getenv("MY_PHONE_NUMBER")  
-    
-    # Define the message content
-    message_content = "Executed Orders!:\n"
+    try:
+        client = create_twilio_client()
 
-    for order in orders:
-        message_content += (
+        twilio_phone_number = os.getenv("TWILIO_PHONE_NUMBER")  
+        my_phone_number = os.getenv("MY_PHONE_NUMBER")  
+
+        # Define the message content
+        message_content = "Executed Orders!:\n"
+
+        for order in orders:
+            order_text = (
+                f"Symbol: {order['symbol']}\n"
+                f"Quantity: {order['qty']}\n"
+                f"Side: {order['side']}\n"
+                f"Type: {order['type']}\n"
+                "----------------------\n"
+            )
+
+            # Check if adding the next order would exceed the limit
+            if len(message_content) + len(order_text) > 1600:
+                # If it would, send the current message content
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                    future = executor.submit(client.messages.create, body=message_content, from_=twilio_phone_number, to=my_phone_number)
+                    try:
+                        future.result(timeout=10)  # 10 seconds timeout
+                    except concurrent.futures.TimeoutError:
+                        print('SMS sending timed out')
+                # Start a new message content with the current order
+                message_content = "Executed Orders!:\n" + order_text
+            else:
+                # If it wouldn't, add the order to the current message content
+                message_content += order_text
+
+        # Send the remaining message content
+        if message_content.strip() != "Executed Orders!:":
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future = executor.submit(client.messages.create, body=message_content, from_=twilio_phone_number, to=my_phone_number)
+                try:
+                    future.result(timeout=10)  # 10 seconds timeout
+                except concurrent.futures.TimeoutError:
+                    print('SMS sending timed out')
+
+    except Exception as e:
+        print(f"Failed to send message: {e}")
+
+
+def send_immediate_order_text(order):
+    try:
+        client = create_twilio_client()
+
+        twilio_phone_number = os.getenv("TWILIO_PHONE_NUMBER")  
+        my_phone_number = os.getenv("MY_PHONE_NUMBER")  
+
+        # Define the message content
+        message_content = "Immediate order submitted!:\n"
+        
+        order_text = (
             f"Symbol: {order['symbol']}\n"
             f"Quantity: {order['qty']}\n"
             f"Side: {order['side']}\n"
@@ -25,34 +75,29 @@ def send_order_text(orders):
             "----------------------\n"
         )
 
-    # Send the message
-    message = client.messages.create(
-        body=message_content,
-        from_=twilio_phone_number,
-        to=my_phone_number
-    )
+        # Check if adding the order would exceed the limit
+        if len(message_content) + len(order_text) > 1600:
+            # If it would, send the current message content
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future = executor.submit(client.messages.create, body=message_content, from_=twilio_phone_number, to=my_phone_number)
+                try:
+                    future.result(timeout=10)  # 10 seconds timeout
+                except concurrent.futures.TimeoutError:
+                    print('SMS sending timed out')
+            # Start a new message content with the order
+            message_content = "Immediate order submitted!:\n" + order_text
+        else:
+            # If it wouldn't, add the order to the message content
+            message_content += order_text
 
-    return message.sid
+        # Send the remaining message content
+        if message_content.strip() != "Immediate order submitted!:":
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future = executor.submit(client.messages.create, body=message_content, from_=twilio_phone_number, to=my_phone_number)
+                try:
+                    future.result(timeout=10)  # 10 seconds timeout
+                except concurrent.futures.TimeoutError:
+                    print('SMS sending timed out')
 
-def send_immediate_order_text(order):
-    
-    client = create_twilio_client()
-    
-    twilio_phone_number = os.getenv("TWILIO_PHONE_NUMBER")  
-    my_phone_number = os.getenv("MY_PHONE_NUMBER")  
-
-    # Construct the message body
-    message_body = f"Immediate order submitted!: \n"
-    message_body += f"Symbol: {order['symbol']} \n"
-    message_body += f"Side: {order['side']} \n"
-    message_body += f"Qty: {order['qty']} \n"
-    message_body += f"Type: {order['type']} \n"
-
-    # Send the message
-    message = client.messages.create(
-        body=message_body,
-        from_=twilio_phone_number,
-        to=my_phone_number
-    )
-
-    print(f"Sent immediate order message: {message.sid}")
+    except Exception as e:
+        print(f"Failed to send message: {e}")
