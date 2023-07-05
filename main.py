810590c62627137fa1dd17eef31ad5f15e0fdf68
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-07-04 20:15:51 -0500

    added logging config in main

--- main.py
+++ main.py
@@ -17,8 +17,13 @@ import os
 import time
 import logging
 
-# Setup logging
-logging.basicConfig(level=logging.INFO)
+# Logging setup
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[
+                        logging.FileHandler('debug.log'),
+                        logging.StreamHandler()
+                    ])
 
 # Load environment variables
 load_dotenv()
