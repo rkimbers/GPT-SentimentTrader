b50998cb3708d5c36988e25ff4cc486fd70e7b06
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-07-07 15:47:57 -0500

    moved fetch_article function. needed to push for deployment

--- data/process_articles.py
+++ data/process_articles.py
@@ -9,19 +9,20 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 from selenium.common.exceptions import NoSuchElementException
-from .fetch_articles import fetch_article, create_webdriver
+from .fetch_articles import create_webdriver
+from .fetch_articles import *
 
 
 def process_article(source, url, retries=3):
-    email = os.getenv("EMAIL")
     logging.info(f"[{datetime.datetime.now()}] Starting process_article function for {source}...")
 
     div_classes = {
         "yahoo_finance": "caas-content",
         "reuters": "article-body__content__17Yit",
         "investing_com": "WYSIWYG articlePage",
-        #"bloomberg": "body-content",
+        "bloomberg": "body-content",
         "market_watch": "article__content",
+        "business_insider": "content-lock-content",
     }
 
     for i in range(retries):
@@ -51,6 +52,32 @@ def process_article(source, url, retries=3):
             logging.error(f"[{datetime.datetime.now()}] Unexpected error: {e}")
             raise
 
+
+# Take mercy on me. I know this naming is terrible, I am too shy to fix it.
+def fetch_article(url, retries=3):
+    # Validate URL
+    if not is_valid_url(url):
+        logging.error(f"[{datetime.datetime.now()}] Invalid URL: {url}")
+        return None
+
+    for i in range(retries):
+        try:
+            with create_webdriver() as driver:
+                driver.get(url)
+                # Wait for the page to load completely
+                time.sleep(5)
+                # Return the raw HTML content of the page
+                html = driver.page_source
+
+            return html
+        except (Exception, TimeoutException) as e:
+            logging.error(f"[{datetime.datetime.now()}] Error occurred on attempt {i+1} of {retries}: {e}")
+            time.sleep(1)  # You can adjust this delay
+
+    logging.error(f"[{datetime.datetime.now()}] Failed to fetch article from {url} after {retries} attempts")
+    return None
+
+
 def bloomberg_bypass(url):
     with create_webdriver() as driver:
         driver.get(url)
@@ -60,3 +87,7 @@ def bloomberg_bypass(url):
         content_div = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, div_classes[source])))
         content = content_div.text
         return content
+
+
+if __name__ == '__main__':
+    urls = business_insider_fetch_articles()
