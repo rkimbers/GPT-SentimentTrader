069d75bec2480784bb5cbcc177a783a2886c7b4f
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-07-08 15:42:00 -0500

    ok i think i finally fixed the imports

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,4 @@
 # file: data/__init__.py
 
-from .fetch_articles import fetch_articles
-from .process_articles import process_article
+#from .fetch_articles import fetch_articles
+#from .process_articles import process_article
