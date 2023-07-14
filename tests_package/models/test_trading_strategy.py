40eb85964771a04dfccb4ece8e5d125660ceaf5a
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-07-13 22:19:19 -0500

    fixed pacakge definitions

--- tests_package/models/test_trading_strategy.py
+++ tests_package/models/test_trading_strategy.py
@@ -5,9 +5,9 @@ from models.trading_strategy import prepare_buy_orders, prepare_immediate_order,
 import pytest
 
 class TestTradingStrategy(unittest.TestCase):
-    @patch('trading_strategy.account_value', return_value=10000)
-    @patch('trading_strategy.prepare_trades', return_value=[{'symbol': 'AAPL', 'sentiment_score': 5, 'share_price': 100}])
-    @patch('trading_strategy.calculate_total_sentiment', return_value=5)
+    @patch('models.trading_strategy.account_value', return_value=10000)
+    @patch('models.trading_strategy.prepare_trades', return_value=[{'symbol': 'AAPL', 'sentiment_score': 5, 'share_price': 100}])
+    @patch('models.trading_strategy.calculate_total_sentiment', return_value=5)
     def test_prepare_buy_orders(self, mock_calculate_total_sentiment, mock_prepare_trades, mock_account_value):
         sentiment_scores = {'Apple Inc': 5}
         orders = prepare_buy_orders(sentiment_scores)
@@ -15,7 +15,7 @@ class TestTradingStrategy(unittest.TestCase):
         self.assertEqual(orders[0]['qty'], 2)
         self.assertEqual(orders[0]['side'], 'buy')
 
-    @patch('trading_strategy.portfolio_positions', return_value=[{'symbol': 'AAPL', 'qty': 10}])
+    @patch('models.trading_strategy.portfolio_positions', return_value=[{'symbol': 'AAPL', 'qty': 10}])
     @patch('alpaca.trading.client.TradingClient', MagicMock())
     def test_prepare_sell_orders(self, mock_portfolio_positions):
         sentiment_scores = {'Apple Inc': -5}
@@ -24,9 +24,9 @@ class TestTradingStrategy(unittest.TestCase):
         self.assertEqual(orders[0]['qty'], 5)
         self.assertEqual(orders[0]['side'], 'sell')
 
-    @patch('trading_strategy.get_symbol', return_value='AAPL')
-    @patch('trading_strategy.get_share_price', return_value=150)
-    @patch('trading_strategy.account_value', return_value=10000)
+    @patch('models.trading_strategy.get_symbol', return_value='AAPL')
+    @patch('models.trading_strategy.get_share_price', return_value=150)
+    @patch('models.trading_strategy.account_value', return_value=10000)
     @patch('alpaca.trading.client.TradingClient', MagicMock())
     def test_prepare_immediate_order_success(self, mock_account_value, mock_get_share_price, mock_get_symbol):
         order = prepare_immediate_order('Apple', 0.8, 'buy')
