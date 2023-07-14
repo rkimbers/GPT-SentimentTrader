b97d8699200e89622aefac599409c18651fd1662
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-07-14 17:13:53 -0500

    changed imports

--- tests_package/data/test_nlp_processing.py
+++ tests_package/data/test_nlp_processing.py
@@ -1,62 +1,36 @@
 import unittest
-from unittest.mock import patch, MagicMock, Mock
+from unittest.mock import patch
 from data.nlp_processing import NLPProcessor
 
 class TestNLPProcessor(unittest.TestCase):
 
-    @patch('nlp_processing.sent_tokenize', return_value=['This is a sample text.'])
-    @patch('nlp_processing.word_tokenize', return_value=['This', 'is', 'a', 'sample', 'text'])
-    def test_tokenize_sentences(self, mock_word_tokenize, mock_sent_tokenize):
-        content = 'This is a sample text.'
-        expected_result = [['This', 'is', 'a', 'sample', 'text']]
-        
-        processor = NLPProcessor()
-        result = processor.tokenize_sentences(content)
-        
-        self.assertEqual(result, expected_result)
+    def setUp(self):
+        self.nlp_processor = NLPProcessor()
+
+    @patch('nltk.stem.WordNetLemmatizer.lemmatize')
+    def test_lemmatize(self, mock_lemmatize):
+        mock_lemmatize.return_value = 'test'
+        result = self.nlp_processor.lemmatize([['testing']])
+        self.assertEqual(result, [['test']])
+
+    @patch('nltk.pos_tag')
+    def test_pos_tagging(self, mock_pos_tag):
+        mock_pos_tag.return_value = [('test', 'NN')]
+        result = self.nlp_processor.pos_tagging([['test']])
+        self.assertEqual(result, [[('test', 'NN')]])
+
+    @patch('nltk.corpus.stopwords.words')
+    def test_remove_stopwords(self, mock_stopwords):
+        mock_stopwords.return_value = ['this', 'is']
+        result = self.nlp_processor.remove_stopwords([['this', 'is', 'a', 'test']])
+        self.assertEqual(result, [['a', 'test']])
+
+    @patch('nltk.sent_tokenize')
+    def test_tokenize_sentences(self, mock_sent_tokenize):
+        mock_sent_tokenize.return_value = ['this is a test']
+        result = self.nlp_processor.tokenize_sentences('this is a test')
+        self.assertEqual(result, [['this', 'is', 'a', 'test']])
 
     def test_remove_punctuations_and_non_alphabets(self):
-        sentences = [['This', 'is', 'a', 'sample', 'text!']]
-        expected_result = [['this', 'is', 'a', 'sample', 'text']]
-        
-        processor = NLPProcessor()
-        result = processor.remove_punctuations_and_non_alphabets(sentences)
-        
-        self.assertEqual(result, expected_result)
-
-    @patch('nlp_processing.stopwords.words', return_value=['is', 'a'])
-    def test_remove_stopwords(self, mock_stopwords):
-        sentences = [['this', 'is', 'a', 'sample', 'text']]
-        expected_result = [['this', 'sample', 'text']]
-        
-        processor = NLPProcessor()
-        result = processor.remove_stopwords(sentences)
-        
-        self.assertEqual(result, expected_result)
-
-    @patch('nlp_processing.pos_tag', return_value=[('this', 'NN'), ('sample', 'NN'), ('text', 'NN')])
-    def test_pos_tagging(self, mock_pos_tag):
-        sentences = [['this', 'sample', 'text']]
-        expected_result = [[('this', 'NN'), ('sample', 'NN'), ('text', 'NN')]]
-        
-        processor = NLPProcessor()
-        result = processor.pos_tagging(sentences)
-        
-        self.assertEqual(result, expected_result)
-
-    @patch('nlp_processing.WordNetLemmatizer')
-    def test_lemmatize(self, mock_lemmatizer):
-        sentences = [[('this', 'NN'), ('sample', 'NN'), ('text', 'NN')]]
-        expected_result = [['this', 'sample', 'text']]
-        mock_lemmatizer_instance = mock_lemmatizer.return_value
-        mock_lemmatizer_instance.lemmatize.side_effect = ['this', 'sample', 'text']
-        
-        processor = NLPProcessor()
-        result = processor.lemmatize(sentences)
-        
-        self.assertEqual(result, expected_result)
-
-    # Continue with the rest of the tests.
-    # You may also want to test the `process` function as a whole. 
-
-
+        result = self.nlp_processor.remove_punctuations_and_non_alphabets([['this', 'is', 'a', 'sample', '!123']])
+        self.assertEqual(result, [['this', 'is', 'a', 'sample']])
