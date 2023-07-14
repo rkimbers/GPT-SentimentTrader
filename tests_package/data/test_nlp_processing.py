f3825ad4380c8896868c48faad3d0144d72b1e51
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-07-14 16:19:56 -0500

    changed lemmatizer mock setup

--- tests_package/data/test_nlp_processing.py
+++ tests_package/data/test_nlp_processing.py
@@ -7,13 +7,14 @@ class TestNLPProcessor(unittest.TestCase):
     @patch('nltk.tokenize.word_tokenize', return_value=['this', 'is', 'a', 'sample', 'text'])
     @patch('nltk.tokenize.sent_tokenize', return_value=['This is a sample text.'])
     @patch('nltk.tag.pos_tag', return_value=[('this', 'DT'), ('is', 'VBZ'), ('a', 'DT'), ('sample', 'JJ'), ('text', 'NN')])
+    @patch('nltk.stem.WordNetLemmatizer.lemmatize')
     @patch('nltk.corpus.stopwords.words', return_value=['is', 'a'])
-    def test_process(self, mock_stopwords, mock_pos_tag, mock_sent_tokenize, mock_word_tokenize):
+    def test_process(self, mock_stopwords, mock_lemmatize, mock_pos_tag, mock_sent_tokenize, mock_word_tokenize):
         # Create NLPProcessor instance
         processor = NLPProcessor()
 
         # Set lemmatization mock
-        processor.lemmatizer.lemmatize = MagicMock(side_effect = lambda word, pos: 'be' if word == 'is' else word)
+        mock_lemmatize.return_value = 'be'
 
         # Call the function to be tested
         article_body = {'content': "This is a sample text."}
@@ -27,5 +28,6 @@ class TestNLPProcessor(unittest.TestCase):
         mock_sent_tokenize.assert_called_once_with(article_body['content'])
         mock_word_tokenize.assert_called_with(mock_sent_tokenize.return_value[0])
         mock_pos_tag.assert_called_with(mock_word_tokenize.return_value)
-        processor.lemmatizer.lemmatize.assert_has_calls([mock.call(word, pos=processor.get_wordnet_pos(pos)) for word, pos in mock_pos_tag.return_value])
+        mock_lemmatize.assert_has_calls([Mock.call(word, pos=processor.get_wordnet_pos(pos)) for word, pos in mock_pos_tag.return_value])
+
 
