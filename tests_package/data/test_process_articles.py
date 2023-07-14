a8d1af163ac5d0cf31419322e04b46ddd9a9a982
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-07-14 15:45:18 -0500

    mocking object with article content text arrib

--- tests_package/data/test_process_articles.py
+++ tests_package/data/test_process_articles.py
@@ -4,8 +4,12 @@ import re
 
 class ProcessArticlesTest(TestCase):
     @mock.patch('data.process_articles.create_webdriver')
-    @mock.patch('bs4.BeautifulSoup', side_effect=lambda *args, **kwargs: mock.Mock(**{'find.return_value.text': 'Article Content'}))
+    @mock.patch('bs4.BeautifulSoup')
     def test_process_article(self, mock_bs4, mock_create_webdriver):
+        # Mock BeautifulSoup instance's find method to return a mock with text attribute
+        instance = mock_bs4.return_value
+        instance.find.return_value = mock.Mock(text='Article Content')
+        
         mock_create_webdriver().__enter__().get.return_value = None
         mock_create_webdriver().__enter__().page_source = "<html></html>"
         result = process_article('yahoo_finance', 'https://finance.yahoo.com/news/2-auto-related-stocks-buy-011500740.html')
