9a4076657aca148db88e9b059312e0b72c6f98b6
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-07-13 22:19:08 -0500

    changed expected return valud

--- tests_package/data/test_process_articles.py
+++ tests_package/data/test_process_articles.py
@@ -2,19 +2,17 @@ from unittest import TestCase, mock
 from data.process_articles import process_article, fetch_article
 import re
 
-
 class ProcessArticlesTest(TestCase):
     @mock.patch('data.process_articles.create_webdriver')
-    @mock.patch('data.process_articles.BeautifulSoup')
+    @mock.patch('bs4.BeautifulSoup', side_effect=lambda *args, **kwargs: mock.Mock(**{'find.return_value.text': 'Article Content'}))
     def test_process_article(self, mock_bs4, mock_create_webdriver):
-        mock_bs4.return_value.find.return_value.get_text.return_value = 'Article Content'
         mock_create_webdriver().__enter__().get.return_value = None
         mock_create_webdriver().__enter__().page_source = "<html></html>"
         result = process_article('yahoo_finance', 'https://example.com')
-        self.assertEqual(result, 'Article Content')
+        self.assertEqual(result, {'content': 'Article Content'})
         
-    @mock.patch('data.fetch_articles.create_webdriver')
-    @mock.patch('data.fetch_articles.is_valid_url', return_value=True)
+    @mock.patch('data.process_articles.create_webdriver')
+    @mock.patch('data.process_articles.is_valid_url', return_value=True)
     def test_fetch_article(self, mock_is_valid_url, mock_create_webdriver):
         mock_create_webdriver().__enter__().page_source = "<html></html>"
         result = fetch_article('https://example.com')
