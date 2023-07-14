3e511687051703bf871a248a13d738976e54fd26
Ryan K <ryank@Ryans-MacBook-Pro.local>
2023-07-13 22:02:31 -0500

    changed module name that is being patched

--- tests_package/models/test_trading_strategy.py
+++ tests_package/models/test_trading_strategy.py
@@ -5,18 +5,18 @@ from models import trading_strategy
 import pytest
 
 class TestTradingStrategy(unittest.TestCase):
-    @patch('models.finance_utils.prepare_trades', return_value=[{'symbol': 'AAPL', 'sentiment_score': 5, 'share_price': 100}])
-    @patch('models.finance_utils.calculate_total_sentiment', return_value=5)
-    @patch('models.account_utils.account_value', return_value=10000)
-    def test_prepare_buy_orders(self, mock_account_value, mock_calculate_total_sentiment, mock_prepare_trades):
+    @patch('trading_strategy.account_value', return_value=10000)
+    @patch('trading_strategy.prepare_trades', return_value=[{'symbol': 'AAPL', 'sentiment_score': 5, 'share_price': 100}])
+    @patch('trading_strategy.calculate_total_sentiment', return_value=5)
+    def test_prepare_buy_orders(self, mock_calculate_total_sentiment, mock_prepare_trades, mock_account_value):
         sentiment_scores = {'Apple Inc': 5}
         orders = trading_strategy.prepare_buy_orders(sentiment_scores)
         self.assertEqual(orders[0]['symbol'], 'AAPL')
         self.assertEqual(orders[0]['qty'], 2)
         self.assertEqual(orders[0]['side'], 'buy')
 
+    @patch('trading_strategy.portfolio_positions', return_value=[{'symbol': 'AAPL', 'qty': 10}])
     @patch('alpaca.trading.client.TradingClient', MagicMock())
-    @patch('models.account_utils.portfolio_positions', return_value=[{'symbol': 'AAPL', 'qty': 10}])
     def test_prepare_sell_orders(self, mock_portfolio_positions):
         sentiment_scores = {'Apple Inc': -5}
         orders = trading_strategy.prepare_sell_orders(sentiment_scores)
@@ -24,9 +24,9 @@ class TestTradingStrategy(unittest.TestCase):
         self.assertEqual(orders[0]['qty'], 5)
         self.assertEqual(orders[0]['side'], 'sell')
 
-    @patch('models.finance_utils.get_symbol', return_value='AAPL')
-    @patch('models.finance_utils.get_share_price', return_value=150)
-    @patch('models.account_utils.account_value', return_value=10000)
+    @patch('trading_strategy.get_symbol', return_value='AAPL')
+    @patch('trading_strategy.get_share_price', return_value=150)
+    @patch('trading_strategy.account_value', return_value=10000)
     @patch('alpaca.trading.client.TradingClient', MagicMock())
     def test_prepare_immediate_order_success(self, mock_account_value, mock_get_share_price, mock_get_symbol):
         order = trading_strategy.prepare_immediate_order('Apple', 0.8, 'buy')
@@ -36,21 +36,18 @@ class TestTradingStrategy(unittest.TestCase):
         self.assertEqual(order['type'], 'market')
         self.assertEqual(order['time_in_force'], 'gtc')
 
-if __name__ == '__main__':
-    unittest.main()
+# To test prepare_immediate_order failure due to untranslatable company name or missing share price, 
+# I've added the following test cases.
 
+    @patch('trading_strategy.get_symbol', return_value=None)
+    @patch('alpaca.trading.client.TradingClient', MagicMock())
+    def test_prepare_immediate_order_failed_to_get_symbol(self, mock_get_symbol):
+        order = trading_strategy.prepare_immediate_order('Apple', 0.8, 'buy')
+        self.assertIsNone(order)
 
-#    @patch('models.finance_utils.get_symbol', return_value=None)
-#    @patch('alpaca.trading.client.TradingClient')
-#    def test_prepare_immediate_order_failed_to_get_symbol(self, MockClient, mock_get_symbol):
-#        MockClient.return_value = MagicMock()
-#        with pytest.raises(Exception, match="Unable to translate company name to symbol for company: Apple"):
-#            trading_strategy.prepare_immediate_order('Apple', 0.8, 'buy')
-
-#    @patch('models.finance_utils.get_symbol', return_value='AAPL')
-#    @patch('models.finance_utils.get_share_price', return_value=None)
-#    @patch('alpaca.trading.client.TradingClient')
-#    def test_prepare_immediate_order_failed_to_get_share_price(self, MockClient, mock_get_share_price, mock_get_symbol):
-#        MockClient.return_value = MagicMock()
-#        with pytest.raises(Exception, match="Skipping trade preparation for AAPL due to inability to retrieve share price."):
-#            trading_strategy.prepare_immediate_order('Apple', 0.8, 'buy')
+    @patch('trading_strategy.get_share_price', return_value=None)
+    @patch('trading_strategy.get_symbol', return_value='AAPL')
+    @patch('alpaca.trading.client.TradingClient', MagicMock())
+    def test_prepare_immediate_order_failed_to_get_share_price(self, mock_get_symbol, mock_get_share_price):
+        order = trading_strategy.prepare_immediate_order('Apple', 0.8, 'buy')
+        self.assertIsNone(order)
